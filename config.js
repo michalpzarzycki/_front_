6d0a8585fb9d175f91ebc375731a2f22ec0fb527
Michal <michal.p.zarzycki@wp.pl>
2021-04-26 15:19:52 +0200

    added env

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 import getConfig from 'next/config';
 
-const { publicRuntimeConfig } = getConfig()
+const { env } = getConfig()
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'https://seoblog.com' : 'http://localhost:8000';
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const API = env.PRODUCTION ? 'https://seoblog.com' : 'http://localhost:8000';
+export const APP_NAME = env.APP_NAME;
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION ? 
-                      publicRuntimeConfig.DOMAIN_PRODUCTION :
-                      publicRuntimeConfig.DOMAIN_DEVELOPMENT
+export const DOMAIN = env.PRODUCTION ? 
+                      env.DOMAIN_PRODUCTION :
+                      env.DOMAIN_DEVELOPMENT
 
-export const DISCUSS_SHORTNAME = publicRuntimeConfig.DISCUSS_SHORTNAME
+export const DISCUSS_SHORTNAME = env.DISCUSS_SHORTNAME
