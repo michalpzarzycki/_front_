ef428a0f87ebc7f4f1a9905c29fb18e0a9e666aa
Michal <michal.p.zarzycki@wp.pl>
2021-04-26 15:24:14 +0200

    Comment blog.js

--- pages/admin/crud/blog.js
+++ pages/admin/crud/blog.js
@@ -1,22 +1,22 @@
-import Layout from '../../../components/Layout'
-import Admin from '../../../components/auth/Admin'
-import BlogCreate from '../../../components/crud/BlogCreate'
-import Link from 'next/link'
+// import Layout from '../../../components/Layout'
+// import Admin from '../../../components/auth/Admin'
+// import BlogCreate from '../../../components/crud/BlogCreate'
+// import Link from 'next/link'
 
 const Blog = () => {
-    return (
-        <Layout>
-            <Admin>
-                <div className="row">
-                    <div className="col-md-12">
-                        <h2>Create a new Blog</h2>
-                    </div>
-                    <div className="col-md-12">
-                       <BlogCreate />
-                    </div>
-                </div>
-            </Admin>
-        </Layout>
+    return (<></>
+        // <Layout>
+        //     <Admin>
+        //         <div className="row">
+        //             <div className="col-md-12">
+        //                 <h2>Create a new Blog</h2>
+        //             </div>
+        //             <div className="col-md-12">
+        //                <BlogCreate />
+        //             </div>
+        //         </div>
+        //     </Admin>
+        // </Layout>
     )
 }
 
